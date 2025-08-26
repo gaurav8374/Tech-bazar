ae218348918bdab34da550d80386ac96b5d07522
gaurav8374 <humagaingaurab@gmail.com>
2025-08-26 11:52:55 +0545

    Delete Tech bazaar directory

--- Tech bazaar/brands.js
+++
@@ -1,8 +0,0 @@
-  const navToggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelector('.nav-links');
-
-  navToggle.addEventListener('click', () => {
-    const expanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
-    navToggle.setAttribute('aria-expanded', !expanded);
-    navLinks.classList.toggle('nav-open');
-  });
